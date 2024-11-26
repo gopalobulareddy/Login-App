626e30788705e88c3c560f41dda548420270e552
gopalobulareddy <gopalobulareddy@gmail.com>
2024-11-26 05:54:43 +0000

    Login-App

---
+++ src/components/Login/index.js
@@ -0,0 +1,26 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Login extends Component {
+  state = {isValue: true}
+  onLoginButton = () => {
+    this.setState(prevState => ({isValue: !prevState.isValue}))
+  }
+  render() {
+    const {isValue} = this.state
+    const headingText = isValue ? 'Please Login' : 'Welcome User'
+    const buttonText = isValue ? 'Login' : 'Logout'
+    return (
+      <div className="bg-container">
+        <div className="cart-container">
+          <h1 className="heading">{headingText}</h1>
+          <button onClick={this.onLoginButton} className="btn-1">
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Login
